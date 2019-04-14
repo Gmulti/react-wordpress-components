190baed80a108a2989601023553050e7f91f27ff
Gmulti <thomasdeneulin@gmail.com>
2019-04-14 14:15:18 +0200

    Add test on wordpress connector

---
+++ packages/wordpress-api/connectors/__tests__/connectWordPress.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Enzyme, { mount, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { connectWordPress } from '../connectWordPress'
+import WordPressProvider from '../../WordPressProvider'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const BASE_URL = 'https://example.com'
+
+describe('connectWordPress', () => {
+    it('provides the correct props to the component', () => {
+        const Fake = () => null
+
+        const ComponentConnected = connectWordPress(Fake)
+
+        const ProviderConnected = () => (
+            <WordPressProvider baseUrl={{ value: BASE_URL }}>
+                <ComponentConnected />
+            </WordPressProvider>
+        )
+
+        const wrapper = mount(<ProviderConnected />)
+
+        expect(wrapper.find(Fake).prop('baseUrl').value).toEqual(BASE_URL)
+    })
+})
