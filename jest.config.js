d4407a4ddf392378d5fad3df46b5d527d9c6550d
Gmulti <thomasdeneulin@gmail.com>
2019-04-18 18:13:03 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-/* eslint-disable import/no-commonjs */
-
 module.exports = {
-    roots: ['<rootDir>/packages'],
-    snapshotSerializers: ['enzyme-to-json/serializer']
+    verbose: true,
+    transformIgnorePatterns: []
 }
