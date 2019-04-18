64bf05267ba44be9e4e29d812e41a59b23aae09e
Gmulti <thomasdeneulin@gmail.com>
2019-04-18 18:15:57 +0200

    Update tests

--- packages/wordpress-api/connectors/__tests__/connectWordPress.js
+++ packages/wordpress-api/connectors/__tests__/connectWordPress.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme, { mount, shallow } from 'enzyme'
+import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { connectWordPress } from '../connectWordPress'
 import WordPressProvider from '../../WordPressProvider'
