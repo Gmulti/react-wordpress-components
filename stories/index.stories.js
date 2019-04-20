6ac6fc15c7dc1166321ce7d2dad333133d91014c
Gmulti <thomasdeneulin@gmail.com>
2019-04-20 13:23:19 +0200

    Update stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import WordPressProvider from '../packages/wordpress-api/WordPressProvider'
+
 import Posts from '../packages/wordpress-dom/widgets/Posts'
 import Comments from '../packages/wordpress-dom/widgets/Comments'
+import WidgetCategories from '../packages/wordpress-dom/widgets/Categories'
 
 storiesOf('Welcome', module).add('React WordPress Components', () => (
     <h1>Welcome to React WordPress Components</h1>
@@ -20,3 +22,15 @@ storiesOf('Comment', module).add('widget', () => (
         <Comments />
     </WordPressProvider>
 ))
+
+storiesOf('Widgets', module)
+    .add('Posts', () => (
+        <WordPressProvider baseUrl="https://essential-dev-skills.com">
+            <WidgetPosts />
+        </WordPressProvider>
+    ))
+    .add('Categories', () => (
+        <WordPressProvider baseUrl="https://essential-dev-skills.com">
+            <WidgetCategories />
+        </WordPressProvider>
+    ))
